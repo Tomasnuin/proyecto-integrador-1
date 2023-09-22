0a56b016aed93272db57f9cc6b05321e7410e376
Tomasnuin <tomasnuingas@gmail.com>
2023-09-21 22:06:30 -0300

    DB agregada, JS agregado

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// Lo primero que quiero hacer es mostrar la info de responsive en la base de datos
+// Despues agregar la interaccion con los botones
+// Despues cambiarlo para que al clickear traiga la info de lo que corresponda
+// si no es muy intenso en el uso de la memoria traigo todo para mostrarlo mas rapido
+// Hacer profiling y evaluar que optimizaciones tengo que hacer
